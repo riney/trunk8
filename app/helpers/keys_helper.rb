f2a99d7525e14a892b141b9b0b1e96ecb7e3be12
riney <john@rineysoft.com>
2009-05-27 00:55:33 -0400

    elephant messages and URL fixes

--- app/helpers/keys_helper.rb
+++ app/helpers/keys_helper.rb
@@ -1,2 +1,7 @@
 module KeysHelper
+  def trunky_message
+    [ "Nom nom nom.", "Mmm, delicious keys.", "Burp.", "I can haz moar plz?", "Mmm. Keys.", "Mmm, spicy keys.",
+      "Nom nom.", "Can't believe I ate the whole key.", "Moar keys plz.", "Om nom nom.", "Grom nom nom.", 
+      "*loud elephanty trumpeting noise*" ].rand
+  end
 end
